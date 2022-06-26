81918248a9bbb333aa89ad12e37465210879ebde
treyamrich <treyamrich@gmail.com>
2022-06-25 18:17:52 -1000

    ManagePO component now has a display state based on add/edit/view/del mode

--- src/pages/user/management/purchase-order/ManagePO.js
+++ src/pages/user/management/purchase-order/ManagePO.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
 import { listPurchaseOrders } from '../../../../graphql/queries';
 
+const initDisplay = {
+    mode: "view-po",
+    po: null
+};
+
 function ManagePO() {
     const [openPO, setOpenPO] = useState([]);
     const [closedPO, setClosedPO] = useState([]);
+    const [display, setDisplay] = useState(initDisplay);
 
     async function fetchPO() {
         try {
@@ -27,38 +33,46 @@ function ManagePO() {
             setOpenPO(openPOData.data.listPurchaseOrders.items);
             setClosedPO(closedPOData.data.listPurchaseOrders.items);
         } catch(e) {
+            alert(e);
             //setOpRes({...opRes, errorMsg:"Error: Could not fetch purchase orders"});
         }
     }
     useEffect(()=>{
-        fetchPO();
-    }, []);
+        if(display === "view-po")
+            fetchPO();
+    }, [display]);
+
     return(
         <div className="po-main-wrapper">
+            {display.mode === "view-po" && (
             <div>
-                <h1>Open</h1>
-                <div className="po-item-wrapper">
-                    {openPO.map((po, index)=> (
-                        <ul key={index}>
-                            <li>Vendor: {po.vendorId}</li>
-                            <li>Date: {po.date}</li>
-                            <li>Number of purchased products: {po.orderedProducts.length}</li>
-                        </ul>
-                    ))}
+                <div>
+                    <h1>Open</h1>
+                    <div className="po-item-wrapper">
+                        {openPO.map((po, index)=> (
+                            <ul key={index} onClick={()=>setDisplay({mode: "edit-po", po: po})}>
+                                <li>Vendor: {po.vendorId}</li>
+                                <li>Date: {po.date}</li>
+                                <li>Number of purchased products: {po.orderedProducts.length}</li>
+                            </ul>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <h1>Closed</h1>
+                    <div className="po-item-wrapper">
+                        {closedPO.map((po, index)=> (
+                            <ul key={index} onClick={()=>setDisplay({mode: "edit-po", po: po})}>
+                                <li>Vendor: {po.vendorId}</li>
+                                <li>Date: {po.date}</li>
+                                <li>Number of purchased products: {po.orderedProducts.length}</li>
+                            </ul>
+                        ))}
+                    </div>
                 </div>
             </div>
-            <div>
-                <h1>Closed</h1>
-                <div className="po-item-wrapper">
-                    {closedPO.map((po, index)=> (
-                        <ul key={index}>
-                            <li>Vendor: {po.vendorId}</li>
-                            <li>Date: {po.date}</li>
-                            <li>Number of purchased products: {po.orderedProducts.length}</li>
-                        </ul>
-                    ))}
-                </div>
-            </div>
+            )}
+            {display.mode === "edit-po" ? <h2>Edit</h2> : null}
         </div>
     );
 }
