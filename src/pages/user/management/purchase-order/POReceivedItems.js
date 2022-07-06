a441e1d0dfe7afff1a71f303a29fa613e0b317c6
Trey Amrich <treyamrich@gmail.com>
2022-07-05 17:32:11 -1000

    Added buttons for adjusting a received POItem

--- src/pages/user/management/purchase-order/POReceivedItems.js
+++ src/pages/user/management/purchase-order/POReceivedItems.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 
+const initAdjState = {
+    editIdx: -1,
+    adjAmt: 0,
+    reason: ""
+}
 function POReceivedItems({ rcvItems, setRcvItems }) {
+    //Adjustments
+    const [adj, setAdj] = useState(initAdjState);
 
+    const {editIdx, adjAmt, reason} = adj;
     return(
         <div>
             <h3>Received Items</h3>
@@ -19,7 +27,37 @@ function POReceivedItems({ rcvItems, setRcvItems }) {
                     <tr key={"rcv-" + idx}>
                         <td>{item.itemCode}</td>
                         <td>{item.receivedDate}</td>
-                        <td>{item.numReceived}</td>
+                        <td>{editIdx !== idx ? 
+                            <div>{item.numReceived}
+                                <button onClick={()=>setAdj({...adj, editIdx: idx})}>
+                                    Tool Box
+                                </button>
+                            </div> :
+                            <div>
+                                <form>
+                                    <label>Adjustment</label>
+                                    <input type="number"
+                                        step="1"
+                                        onChange={(e)=>setAdj({...adj, adjAmt: e.target.value})}
+                                        value={adjAmt}
+                                    />
+                                    <button type="button" 
+                                        onClick={()=>setAdj({...adj, adjAmt: parseInt(adjAmt)+1})}>+</button>
+                                    <button type="button"
+                                        onClick={()=>setAdj({...adj, adjAmt: adjAmt-1})}>-</button>
+                                    <label>Reason</label>
+                                    <input type="text" placeholder="Miscounted, lost, etc."
+                                        onChange={(e)=>setAdj({...adj, reason: e.target.value})}
+                                        value={reason}
+                                    />
+                                    <button type="button">Confirm</button>
+                                    <button type="button"
+                                        onClick={()=>setAdj({...adj, editIdx: -1, adjAmt: 0})}>Cancel
+                                    </button>
+                                </form>
+                            </div>
+                            }
+                        </td>
                     </tr>
                 ))}
                 </tbody>
