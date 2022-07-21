4b70c8321eeb46ac12e9ad98e51a0abef0d1c6ba
Trey Amrich <treyamrich@gmail.com>
2022-07-21 11:42:14 -1000

    Modularized checkbox component

--- src/components/Table/TableCheckBox.jsx
+++ src/components/Table/TableCheckBox.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 import { MdCheckBoxOutlineBlank, MdCheckBox } from 'react-icons/md';
 import { useStateContext } from '../../contexts/ContextProvider';
 
+const checkBoxClass = "m-auto inline-block cursor-pointer";
+const checkBoxSize = 25;
+
 const TableCheckBox = ({checked, customFunc}) => {
   const { currentColor } = useStateContext();
   return (
     <>
         {checked ? 
           <MdCheckBox
-            className="m-auto inline-block cursor-pointer"
-            size={20}
+            className={checkBoxClass}
+            size={checkBoxSize}
             style={{color: currentColor}}
             onClick={customFunc}
           /> :
           <MdCheckBoxOutlineBlank 
-            className="m-auto inline-block cursor-pointer"
-            size={20}
+            className={checkBoxClass}
+            size={checkBoxSize}
             onClick={customFunc}
           />
         }
