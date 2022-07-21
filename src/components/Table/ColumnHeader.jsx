f80956448f6acf11cf65c7e64fd4aa6ad9ffb427
treyamrich <treyamrich@gmail.com>
2022-07-20 19:34:54 -1000

    Styled some of the table component

--- src/components/Table/ColumnHeader.jsx
+++ src/components/Table/ColumnHeader.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 import { useTableContext } from './contexts/TableContext';
 
-const ColumnHeader = ({headerText, width, height, type, textAlign}) => {
+const ColumnHeader = ({headerText, type, textAlign}) => {
     const { handleSelAll, allSel } = useTableContext();
     
     return (
-        <th style={{ width, height, textAlign}}>
+        <th role="columnheader" 
+            style={{textAlign}}
+            className="text-gray-500 text-sm pr-1 cursor-pointer font-bold overflow-hidden"
+        >
             {type === 'checkbox' ? 
                 <input type="checkbox" 
                     name="checkbox-select-all" 
-                    className="checkbox"
+                    className="checkbox w-5 h-5"
                     onChange={(e)=>handleSelAll(e.currentTarget.checked)}
-                    checked={allSel}/> :
-                headerText
+                    checked={allSel}
+                    /> :
+                <span>{headerText}</span>
             }
         </th>
     )
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -4,38 +4,42 @@ import { useTableContext } from './contexts/TableContext';
 
 const Table = ({data, children}) => {
     
-    const [visibleRecords, setVisibleRecords] = useState([]);
     const { setAllSel, numSel, handleSel } = useTableContext();
     const childArr = Children.toArray(children);
 
     useEffect(()=>{
-        setAllSel(visibleRecords.length === numSel && numSel !== 0);
+        setAllSel(data.length === numSel && numSel !== 0);
     }, [numSel]);
-    useEffect(()=>{
-        setVisibleRecords([...data]);
-    }, []);
 
     return(
-        <div className="table-wrapper">
+        <div className="table-wrapper overflow-x-hidden">
             <TableToolbar />
-            <table className="inventory-items" style={{ border: '1px black solid'}}>
+            <table className="inventory-items border-slate-300 border-1">
+                <colgroup>
+                    {childArr.map((colChild, idx)=> (
+                        <col key={idx} style={{width: colChild.props.width + 'px'}} />
+                    ))}
+                </colgroup>
                 <thead>
                     <tr>
                         {children}
                     </tr>
                 </thead>
                 <tbody>
-                {visibleRecords.length === 0 ? <tr><td colSpan="10" style={{padding: "10px", textAlign:"center"}}> No records </td></tr> : null}
+                {data.length === 0 ? <tr><td colSpan="10" style={{padding: "10px", textAlign:"center"}}> No records </td></tr> : null}
                 {
-                    visibleRecords.map((record, index) => (
-                        <tr key={index}>
+                    data.map((record, index) => (
+                        <tr key={index} className="border-slate-300 border-1">
                             {childArr.map((colChild, idx)=>(
-                                <td key={colChild.props.field + '-' + idx}>
+                                <td key={colChild.props.field + '-' + idx}
+                                    style={{textAlign: colChild.props.textAlign}}
+                                    className="p-1 text-sm overflow-hidden"
+                                >
                                     {colChild.props.type === 'checkbox' && (
                                         <input type="checkbox" 
                                             name="checkbox-item"
                                             id={"checkbox-"+ index}
-                                            className="checkbox" 
+                                            className="checkbox w-5 h-5" 
                                             value={index} 
                                             onChange={(e)=>handleSel(e.target)}
                                         />
