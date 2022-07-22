2b1bf1d399f19f648eaac753fd327d46d776639b
Trey Amrich <treyamrich@gmail.com>
2022-07-21 17:31:19 -1000

    SearchBar now updates the array in real time

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -8,50 +8,50 @@ const SearchBar = ({color, records, setRecords, searchFields}) => {
   const [focused, setFocused] = useState(false);
 
   const handleSearch = (e) => {
-    e.preventDefault();
+    let newQuery = e.target.value;
+    setQuery(newQuery);
+
     //If search is empty, simply reset the records
-    if(query === '') {
-      setRecords([...records, ...filteredRecords]);
+    let allRecs = [...records, ...filteredRecords];
+    if(newQuery === '') {
+      setRecords(allRecs);
       setFilteredRecords([]);
       return;
     }
     //Filter records
     let match;
     let newRecs = [];
-    let newFilteredRecs = [...filteredRecords];
-    let searchRegEx = new RegExp('^' + query, "i")
-    for(let i = 0; i < records.length; i++) {
+    let newFilteredRecs = [];
+    for(let i = 0; i < allRecs.length; i++) {
       match = false;
       for(let j = 0; j < searchFields.length; j++) {
-        if(String(records[i][searchFields[j]]).search(searchRegEx) >= 0) {
+        if(String(allRecs[i][searchFields[j]]).toLowerCase().startsWith(newQuery.toLowerCase())) {
           match = true;
           break;
         }
       }
       !match ? 
-        newFilteredRecs.push(records[i]) :
-        newRecs.push(records[i]);
+        newFilteredRecs.push(allRecs[i]) :
+        newRecs.push(allRecs[i]);
     }
     setRecords(newRecs);
     setFilteredRecords(newFilteredRecs);
   }
   return (
-    <div className="relative h-7 leading-6 border-0"
+    <div className="relative h-7 leading-6 border-0 flex"
     >
-      <form onSubmit={handleSearch} className="flex mt-1">
-        <input 
-          type="text"
-          value={query}
-          onChange={(e)=>setQuery(e.target.value)}
-          className="outline-0 placeholder:text-slate-300 w-max text-xs mr-1"
-          placeholder="Search"
-          onFocus={()=>setFocused(true)}
-          onBlur={()=>setFocused(false)}
-        />
-        <button type="submit">
-          <AiOutlineSearch/> 
-        </button>
-      </form>
+      <input 
+        type="text"
+        value={query}
+        onChange={handleSearch}
+        className="outline-0 placeholder:text-slate-300 w-max text-xs mr-1"
+        placeholder="Search"
+        onFocus={()=>setFocused(true)}
+        onBlur={()=>setFocused(false)}
+      />
+      <button type="submit">
+        <AiOutlineSearch/> 
+      </button>
       <div 
         className={`${focused ? 'search-focus' : ''} search-unfocus`}
         style={{background: color}}
--- src/pages/dashboard/Customers.jsx
+++ src/pages/dashboard/Customers.jsx
@@ -6,7 +6,7 @@ import { Header } from '../../components';
 
 const Customers = () => {
   const selectionsettings = { persistSelection: true };
-  const toolbarOptions = ['Delete', 'Search'];
+  const toolbarOptions = ['Delete', 'Search', 'Add', 'Update'];
   const editing = { allowDeleting: true, allowEditing: true };
 
   return (
