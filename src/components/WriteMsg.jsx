2a324a87547bce88dd01e8b1cd9f7d4b0f405e09
treyamrich <treyamrich@gmail.com>
2022-09-01 17:32:47 -1000

    Remove RTE

--- src/components/WriteMsg.jsx
+++ src/components/WriteMsg.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 
 const WriteMsg = () => {
+  const handleType = () => {
+    console.log('hi')
+  }
   return (
     <div>
-      <RichTextEditorComponent height={450}>
+      <RichTextEditorComponent height={450} onChange={handleType}>
         <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
-    </RichTextEditorComponent>
+      </RichTextEditorComponent>
     </div>
   )
 }
