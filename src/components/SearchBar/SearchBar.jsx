96477a9f043c4adec9eee6585c885ee57e2eb1d0
Trey Amrich <treyamrich@gmail.com>
2022-12-16 17:03:33 -1000

    Added a MyInput component, which is a custom styled input

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai';
-import './searchbar.css';
+import { MyInput } from '..';
 
 const SearchBar = ({color, records, setRecords, searchFields}) => {
   const [query, setQuery] = useState("");
   const [filteredRecords, setFilteredRecords] = useState([]);
-  const [focused, setFocused] = useState(false);
 
   const handleSearch = (e) => {
     let newQuery = e.target.value;
@@ -38,25 +37,18 @@ const SearchBar = ({color, records, setRecords, searchFields}) => {
     setFilteredRecords(newFilteredRecs);
   }
   return (
-    <div className="relative h-7 leading-6 border-0 flex"
-    >
-      <input 
-        type="text"
-        value={query}
-        onChange={handleSearch}
-        className="outline-0 placeholder:text-slate-300 w-max text-xs mr-1 px-2"
-        placeholder="Search"
-        onFocus={()=>setFocused(true)}
-        onBlur={()=>setFocused(false)}
-      />
+    <MyInput
+      type="text"
+      value={query}
+      onChange={handleSearch}
+      placeholder="Search"
+      color={color}
+      componentBtn={
       <button type="button">
         <AiOutlineSearch/> 
       </button>
-      <div 
-        className={`${focused ? 'search-focus' : ''} search-unfocus`}
-        style={{background: color}}
-      />
-    </div>
+      }
+    />
   )
 }
 
