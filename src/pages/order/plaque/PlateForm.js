a056fd186df33e8818f26332092aafd90251bf15
Trey Amrich <treyamrich@gmail.com>
2022-12-25 15:51:01 -1000

    Fixed plateform add graphic feature

--- src/pages/order/plaque/PlateForm.js
+++ src/pages/order/plaque/PlateForm.js
@@ -69,7 +69,7 @@ const PlateForm = ({ btnBgColor, submitForm, managePopUp, editPlate }) => {
 
     const handleAddPltGraphic = graphicObj => {
         popPopUp();
-        setPlate({...plate, graphics: [graphicObj, ...plate.pltGraphics]});
+        setPlate({...plate, pltGraphics: [graphicObj, ...plate.pltGraphics]});
     };
     const confirmPlateMsg = () => {
         managePopUp.pushPopUp(<ConfirmPopUp
@@ -86,6 +86,7 @@ const PlateForm = ({ btnBgColor, submitForm, managePopUp, editPlate }) => {
         cartItem.name = plate.pltSize === 'Custom' ? 
             `Custom ${plate.customW}x${plate.customH}" ${plate.pltColor} plate` :
             `${plate.pltSize} ${plate.pltColor} plate`;
+        cartItem.graphics = plate.pltGraphics;
         submitForm(cartItem);
     }
   return (
