6e445f530eaa95dd34b0ca846e8652d4ecef8fea
Trey Amrich <treyamrich@gmail.com>
2022-12-29 11:56:02 -1000

    Added a order total calculation function to Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,7 +3,6 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
 
 import { useStateContext } from '../contexts/ContextProvider';
-import { cartData } from '../data/dummy';
 import { Button } from '.';
 
 const InitOrderCost = {
@@ -14,7 +13,32 @@ const InitOrderCost = {
 const Cart = ({ order, setOrder }) => {
   const { currentColor } = useStateContext();
   const [orderCost, setOrderCost] = useState(InitOrderCost);
+
   const {subtotal, total} = orderCost;
+  const cart = order.cart;
+  const addonFields = ['graphics', 'cutouts', 'services'];
+
+  const getTotalItemPrice = item => {
+    let itemCost = item.price;
+    addonFields.forEach(addonField => {
+      item[addonField].forEach(addon => itemCost += addon.price);
+    });
+    return itemCost;
+  }
+  const getSubTotal = items => {
+    let total = 0;
+    let itemPrice = 0;
+    for(let i = 0; i < items.length; i++) {
+      itemPrice = getTotalItemPrice(items[i]);
+      //Only recurse when there's a child item
+      if(items[i].subItems.length > 0)
+        itemPrice += getSubTotal(items[i].subItems);
+      total += itemPrice * items[i].quantity;
+    }
+  }
+  const formatItemCategory = category => {
+    return `${category.substring(0,1)}${category.substring(1,category.length).toLowerCase()}`;
+  }
   return (
     <div className="bg-half-transparent w-full fixed nav-item top-0 right-0 ">
       <div className="float-right h-screen  duration-1000 ease-in-out dark:text-gray-200 transition-all dark:bg-[#484B52] bg-white md:w-400 p-8">
@@ -28,14 +52,19 @@ const Cart = ({ order, setOrder }) => {
             borderRadius="50%"
           />
         </div>
-        {cartData.map((item, index) => (
+        {cart.length === 0 && (
+          <div className="text-center leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4">
+            No Items
+          </div>
+        )}
+        {cart.map((item, index) => (
           <div key={index}>
             <div>
               <div className="flex items-center   leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4">
                 <img className="rounded-lg h-80 w-24" src={item.image} alt="" />
                 <div>
                   <p className="font-semibold ">{item.name}</p>
-                  <p className="text-gray-600 dark:text-gray-400 text-sm font-semibold">{item.category}</p>
+                  <p className="text-gray-600 dark:text-gray-400 text-sm font-semibold">{formatItemCategory(item.category)}</p>
                   <div className="flex gap-4 mt-2 items-center">
                     <p className="font-semibold text-lg">{item.price}</p>
                     <div className="flex items-center border-1 border-r-0 border-color rounded">
